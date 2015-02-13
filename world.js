1689b68dd1b44d02c9f13d06b2a64f569f21f9a3
ilya3d <ilya@twinscom.ru>
2015-02-13 14:16:49 +0300

    player test

--- world.js
+++ world.js
@@ -1,10 +1,30 @@
 (function(){
 
+
+    var Player = {
+        name:'hohoho',
+        texture: 'enemy',
+        x:100, y: 250, w:16, h:16,
+        update: function() {
+            return true;
+        },
+
+        draw: function() {
+            ICE.texture.drawRect( 'player', this.x, this.y, this.w, this.h );
+        }
+    };
+
+
     var World = {
+
+        player: null,
 
         init: function() {
 
             ICE.texture.load( 'bg', '1.png' );
+            ICE.texture.load( 'player', '2.png' );
+
+            this.player = Player;
         },
 
         update: function() {
@@ -15,6 +35,7 @@
         draw: function() {
 
             ICE.texture.drawRect( 'bg', 0, 0, 800, 600 );
+            this.player.draw();
             ICE.draw.text( 'mySpace', 700, 10, 16 );
         }
 
